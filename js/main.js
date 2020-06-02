3c1effe5846ef82ca98ecb48734eeecb015c8c80
Wua Fong Tan <wuafong92@live.com.sg>
2020-06-02 12:11:42 +0800

    link date created and link image added.

--- js/main.js
+++ js/main.js
@@ -10,14 +10,14 @@ app = new Vue({
 		links:[
 			{
 				name:"test1",
-				iconPath:"http://",
-				linkPath:"dsagsdg",
+				iconPath:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+				linkPath:"https://www.google.com/search?q=insert+css+in+html&rlz=1C1CHBF_enSG892SG892&oq=insert+css+in+html&aqs=chrome..69i57j0l7.6727j0j7&sourceid=chrome&ie=UTF-8",
 				dateCreated:"dsgsdag",
 			},
 			{
 				name:"adsgsdgds",
-				iconPath:"asdgasdg",
-				linkPath:"sdagsdg",
+				iconPath:"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png",
+				linkPath:"https://www.google.com/search?q=insert+css+in+html&rlz=1C1CHBF_enSG892SG892&oq=insert+css+in+html&aqs=chrome..69i57j0l7.6727j0j7&sourceid=chrome&ie=UTF-8",
 				dateCreated:"asdgasdgdsg",
 			},
 		],
@@ -25,7 +25,7 @@ app = new Vue({
 			name:"",
 			iconPath:"",
 			linkPath:"",
-			dataCreated:"",
+			// dataCreated:"",
 		},
 		testData:"Welcome!",
 		fields:[
@@ -44,7 +44,8 @@ app = new Vue({
 	},
 	methods:{
 		addLink: function(){
-			this.links.push(this.workLink);
+			let theDate = new Date();
+			this.links.push({...this.workLink,dateCreated:theDate});
 			this.workLink = cleanForm;
 		},
 		deleteLink: function(index){
@@ -59,7 +60,7 @@ app = new Vue({
 			this.editing = false;
 		},
 		editLink:function(){
-			this.links[this.toEdit] = {...this.workLink};
+			this.links[this.toEdit] = {...this.workLink,dateCreated:this.links[this.toEdit].dateCreated};
 			this.editing = false;
 			this.workLink = cleanForm;
 		}
